9d539f6d2832cedd58f25ec8e675ec206cced4fd
Mishika Singh Gaur <msgaur1997@gmail.com>
2024-08-05 23:43:54 +0100

    Adding restaurant management system

---
+++ restaurant-customer-management-system/src/main/java/com/springlearning/msg/Application.java
@@ -0,0 +1,13 @@
+package com.springlearning.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+            /*log.info("Application started successfully");*/
+        }
+    }
+
